46451e58d68987125640831996675f617066c3e0
metallic-lighght <159230242+metallic-lighght@users.noreply.github.com>
2024-08-26 21:54:42 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_sockets import Sockets
 from websocket_server import GameWebSocketServer
-
+import os
 app = Flask(__name__)
 sockets = Sockets(app)
 
@@ -16,5 +16,6 @@ def game_socket(ws):
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
